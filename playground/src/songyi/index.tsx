6acd1d31a23c9fd543c6660d59af4cf11da3fc34
songyi <jrl103@naver.com>
2024-07-14 20:23:22 +0900

    타입스크립트 공부

---
+++ playground/src/songyi/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Router from './Router';
+
+export default function Main() {
+  return (
+    <S.Main>
+      <Router />
+    </S.Main>
+  );
+}
+
+const S = {
+  Main: styled.div``,
+};

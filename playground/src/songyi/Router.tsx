cb1b900c8c54fbbaeac893a4f184018835692fdc
songyi <jrl103@naver.com>
2024-07-21 20:17:24 +0900

    2주차 과제

--- playground/src/songyi/Router.tsx
+++ playground/src/songyi/Router.tsx
@@ -4,6 +4,9 @@ import Home from './pages';
 import Chapter1 from './pages/Chapter1';
 import Chapter2 from './pages/Chapter2';
 import Chapter3 from './pages/Chapter3';
+import Chapter4 from './pages/Chapter4';
+import Chapter5 from './pages/Chapter5';
+import Chapter6 from './pages/Chapter6';
 
 export default function Router() {
   return (
@@ -12,6 +15,9 @@ export default function Router() {
       <Route path={routerPath.CHAPTER_1} element={<Chapter1 />} />
       <Route path={routerPath.CHAPTER_2} element={<Chapter2 />} />
       <Route path={routerPath.CHAPTER_3} element={<Chapter3 />} />
+      <Route path={routerPath.CHAPTER_4} element={<Chapter4 />} />
+      <Route path={routerPath.CHAPTER_5} element={<Chapter5 />} />
+      <Route path={routerPath.CHAPTER_6} element={<Chapter6 />} />
     </Routes>
   );
 }
--- playground/src/songyi/constants/routerPath.ts
+++ playground/src/songyi/constants/routerPath.ts
@@ -4,6 +4,8 @@ const routerPath = {
   CHAPTER_2: 'chapter2',
   CHAPTER_3: 'chapter3',
   CHAPTER_4: 'chapter4',
+  CHAPTER_5: 'chapter5',
+  CHAPTER_6: 'chapter6',
 };
 
 export default routerPath;
